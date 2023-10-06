f2cd793ad9732c11c0c6ace91a556cff324c9dea
aelious <aeliousx@gmail.com>
2023-10-05 19:43:58 -0500

    Fixed?

--- imgText.go
+++ imgText.go
@@ -25,22 +25,19 @@ func CreatePicText() {
 		panic(err)
 	}
 
-	image, err := gg.NewContextForImage(dc)
+	image := gg.NewContextForImage(dc)
 
-	if err != nil {
-		panic(err)
-	}
 	// Get image bounds
 	bounds := dc.Bounds()
 	// Assign bounds to W and H
 	W, H := bounds.Max.X, bounds.Max.Y
 
-	if err := dc.LoadFontFace(tempFile.Name(), 72); err != nil {
+	if err := image.LoadFontFace(tempFile.Name(), 72); err != nil {
 		panic(err)
 	}
 
 	image.SetRGB(.5, 0, 0)
-	image.DrawStringAnchored("Hello, world!", W/2, H/2, 0.5, 0.5)
+	image.DrawStringAnchored("Hello, world!", float64(W/2), float64(H/2), 0.5, 0.5)
 	image.Stroke()
 
 	image.SavePNG("hello.png")
